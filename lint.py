0f3570fba5627468909824319e115f5ddbf3efa9
Jessy <ges13596@gmail.com>
2025-01-23 11:48:55 -0500

    chore: initial setup of the program code

---
+++ lint.py
@@ -0,0 +1,52 @@
+"""
+The scripts run the linting in all the files of the proof-of-concept program.
+
+The script fails if the score obtained by pylint is lower than
+a defined thereshold.
+
+For each step, specific disabled rules has been added.
+"""
+
+import sys
+from pylint import lint
+
+THERESHOLD = 9.5
+DISABLE = '--disable=too-many-arguments,wrong-import-order,redefined-builtin'
+
+
+def lint_component():
+    """
+    Checks if all the files follow the linting guidelines from `pylint`.
+    """
+
+    print('Running linter...')
+
+    files = [
+        'main.py',
+        'lint.py',
+        'tests/test_merger.py'
+    ]
+    scores = []
+
+    for file in files:
+        print(f'Running linter on file: {file}')
+        run = lint.Run([file, DISABLE], None, False)
+        scores.append(run.linter.stats.global_note)
+
+    print('Linter execution complete!')
+    print('Performing checks')
+
+    has_failed = False
+    for i, file in enumerate(files):
+        if scores[i] < THERESHOLD:
+            print(f'The file {file} has failed linting check!')
+            has_failed = True
+
+    if has_failed:
+        sys.exit(1)
+
+    print('linting succeeded!')
+
+
+if __name__ == '__main__':
+    lint_component()
---
+++ main.py
@@ -0,0 +1,18 @@
+"""
+The main module contains the main program of the indicator collector.
+
+A typical program execution will query Eurostat databases and merge the
+collected datasets into one file. The program will then save the file.
+"""
+
+def main():
+    """
+    Main execution of the program.
+    """
+
+    print('Programme de collecte de données associées aux indicateurs')
+    print('Ce programme collecte tous les jeux de données considérés pour le volet indicateurs.')
+    print('Ce programme est en cours de développement')
+
+if __name__ == '__main__':
+    main()
